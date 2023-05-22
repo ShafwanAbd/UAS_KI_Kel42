afdc00516e5e295312b773a2780b1e952f77c0b0
ShafwanAbd <217006104@student.unsil.ac.id>
2023-05-22 11:07:26 +0700

    p

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use phpseclib3\Crypt\DSA;
 
 class RegisterController extends Controller
 {
@@ -64,10 +65,14 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        $private = ::createKey(2048, 160);
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'privateKey' => $private,
+            'publicKey' => $private->getPublicKey(),
         ]);
     }
 }
