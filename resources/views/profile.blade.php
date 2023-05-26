e004296bae1b249c3995eaef78b21b32c7be0ac8
RizkiPrtma1 <217006106@student.unsil.ac.id>
2023-05-26 23:00:20 +0700

    fe profil gesss

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,136 +2,162 @@
 
 @section('content')
 
-@if(Session::has('success'))
+<div class="container d-flex justify-content-center align-items-center" style="max-width: 1200px; min-height: 1200px; margin-top: -30px;">
+    @if(Session::has('success'))
     <p class="alert alert-success" id="sixSeconds">{{ Session::get('success') }}</p>
-@elseif (Session::has('failed'))
+    @elseif (Session::has('failed'))
     <p class="alert alert-danger" id="sixSeconds">{{ Session::get('failed') }}</p>
-@endif 
+    @endif
 
-<form class="" method="POST" action="{{ url('profile/update') }}">
-@csrf
-    <input name="name" type="text" value="{{ $user->name }}">
-    <input name="email" type="text" value="{{ $user->email }}">  
-<button type="submit" class="btn btn-primary">Update</button>
+    <div class="card border-0 shadow p-5" style="width: 32rem; ">
+        <div class="card body top p-3">
+            <h5 class="my-3 fw-bold">Profile</h5>
+            <div class="row text-center justify-content-center align-items-center">
+                <form class="" method="POST" action="{{ url('profile/update') }}">
+                    @csrf
+                    <div class="mb-3 text-start">
+                        <label for="name" class="pb-1">Nama Pengguna:</label>
+                        <input name="name" id="name" class="form-control " type="text" value="{{ $user->name }}" style="width: 24rem; ">
 
-<form method="POST" action="{{ url('profile/updatePassword') }}">
-@csrf
-    <div class="">
-        <div class="container_form flex">
-                <div class="container_form_label">
-                    <input id="hashedPassword" type="hidden" value="{{ Auth::user()->password }}"> 
-                    <div class="item flex">
-                        <p>Kata Sandi Sekarang</p>
-                        <div class="input-group">
-                            <span id="hideButton1" class="input-group-text"><img class="animate__animated animate__flipInX" id="hideImage1" src="{{ asset('./image/icon/view_grey.png') }}"></span>
-                            <input id="currentPassword" class="form-control" name="current_pw" placeholder="Password Aktif" type="password" required> 
-                        </div>
                     </div>
-                    <div class="item flex">
-                        <p>Kata Sandi Baru</p>
-                        <div class="input-group">
-                            <span id="hideButton2" class="input-group-text"><img class="animate__animated animate__flipInX" id="hideImage2" src="{{ asset('./image/icon/view_grey.png') }}"></span>
-                            <input id="newPassword" class="form-control" name="new_pw" placeholder="Password Baru" type="password" required> 
-                        </div>
+                    <div class="mb-3 text-start">
+                        <label for="name" class="pb-1">Email:</label>
+                        <input name="email" type="text" class="form-control" value="{{ $user->email }}" style="width: 24rem;">
                     </div>
-                    <div class="item flex">
-                        <p>Konfirmasi Kata Sandi</p>
-                        <div class="input-group">
-                            <span id="hideButton3" class="input-group-text"><img class="animate__animated animate__flipInX" id="hideImage3" src="{{ asset('./image/icon/view_grey.png') }}"></span>
-                            <input id="newPasswordConfirm" class="form-control" name="new_pw_confirm" placeholder="Password Baru" type="password" required> 
-                        </div> 
-                    </div>
-                </div> 
+                    <button type=" submit" class="btn btn-primary px-4">Update</button>
 
-                <script>
-                    $(document).ready(function() {
-                        $('#hideButton1').click(function() {
-                            var imageElement = $('#hideImage1');
-                            var passwordField = $('#currentPassword');
-                            var passwordFieldType = passwordField.attr('type');
-                            
-                            if (passwordFieldType === 'password') {
-                            passwordField.attr('type', 'text');
-                            imageElement.attr('src', '{{asset("./image/icon/view_grey_closed.png")}}')
-                            } else {
-                            passwordField.attr('type', 'password');
-                            imageElement.attr('src', "{{asset('./image/icon/view_grey.png')}}")
-                            }  
-
-                            imageElement.toggleClass('animate__animated animate__flipInX');  
-                            setTimeout(function() {
-                                imageElement.toggleClass('animate__animated animate__flipInX');  
-                            }, 0);
-                            
-                        });
-                        $('#hideButton2').click(function() {
-                            var imageElement = $('#hideImage2');
-                            var passwordField = $('#newPassword');
-                            var passwordFieldType = passwordField.attr('type');
-                            
-                            if (passwordFieldType === 'password') {
-                            passwordField.attr('type', 'text'); 
-                            imageElement.attr('src', '{{asset("./image/icon/view_grey_closed.png")}}')
-                            } else {
-                            passwordField.attr('type', 'password');
-                            imageElement.attr('src', "{{asset('./image/icon/view_grey.png')}}")
-                            }
-
-                            imageElement.toggleClass('animate__animated animate__flipInX');  
-                            setTimeout(function() {
-                                imageElement.toggleClass('animate__animated animate__flipInX');  
-                            }, 0);
-                        });
-                        $('#hideButton3').click(function() {
-                            var imageElement = $('#hideImage3');
-                            var passwordField = $('#newPasswordConfirm');
-                            var passwordFieldType = passwordField.attr('type');
-                            
-                            if (passwordFieldType === 'password') {
-                            passwordField.attr('type', 'text');
-                            imageElement.attr('src', '{{asset("./image/icon/view_grey_closed.png")}}')
-                            } else {
-                            passwordField.attr('type', 'password');
-                            imageElement.attr('src', "{{asset('./image/icon/view_grey.png')}}")
-                            }
-
-                            imageElement.toggleClass('animate__animated animate__flipInX');  
-                            setTimeout(function() {
-                                imageElement.toggleClass('animate__animated animate__flipInX');  
-                            }, 0);
-                        });
-                    });
-                </script> 
+                </form>
             </div>
-        <button type="submit" class="btn btn-primary">Update</button>
+
+        </div>
+        <div class="card body mid p-3 mt-2">
+            <h5 class="my-3 fw-bold">Kata Sandi</h5>
+            <form method="POST" action="{{ url('profile/updatePassword') }}">
+                @csrf
+                <div class="">
+                    <div class="container_form flex">
+                        <div class="container_form_label">
+                            <input id="hashedPassword" type="hidden" value="{{ Auth::user()->password }}">
+                            <div class="item flex">
+
+                                <label for="currentPassword" class="pb-1">Kata Sandi Sekarang:</label>
+                                <div class="input-group mb-3">
+                                    <span id="hideButton1" class="input-group-text"><img class="animate__animated animate__flipInX" id="hideImage1" src="{{ asset('./image/icon/view_grey.png') }}" width="20px" height="auto"></span>
+                                    <input id="currentPassword" class="form-control" name="current_pw" placeholder="Password Aktif" type="password" required>
+                                </div>
+                            </div>
+                            <div class="item flex">
+                                <label for="currentPassword" class="pb-1">Kata Sandi Baru:</label>
+                                <div class="input-group mb-3">
+                                    <span id="hideButton2" class="input-group-text"><img class="animate__animated animate__flipInX" id="hideImage2" src="{{ asset('./image/icon/view_grey.png') }}" width="20px" height="auto"></span>
+                                    <input id="newPassword" class="form-control" name="new_pw" placeholder="Password Baru" type="password" required>
+                                </div>
+                            </div>
+                            <div class="item flex">
+                                <label for="currentPassword" class="pb-1">Konfirmasi Kata Sandi:</label>
+                                <div class="input-group mb-3">
+                                    <span id="hideButton3" class="input-group-text"><img class="animate__animated animate__flipInX" id="hideImage3" src="{{ asset('./image/icon/view_grey.png') }}" width="20px" height="auto"></span>
+                                    <input id="newPasswordConfirm" class="form-control" name="new_pw_confirm" placeholder="Password Baru" type="password" required>
+                                </div>
+                            </div>
+                        </div>
+
+                        <script>
+                            $(document).ready(function() {
+                                $('#hideButton1').click(function() {
+                                    var imageElement = $('#hideImage1');
+                                    var passwordField = $('#currentPassword');
+                                    var passwordFieldType = passwordField.attr('type');
+
+                                    if (passwordFieldType === 'password') {
+                                        passwordField.attr('type', 'text');
+                                        imageElement.attr('src', '{{asset("./image/icon/view_grey_closed.png")}}')
+                                    } else {
+                                        passwordField.attr('type', 'password');
+                                        imageElement.attr('src', "{{asset('./image/icon/view_grey.png')}}")
+                                    }
+
+                                    imageElement.toggleClass('animate__animated animate__flipInX');
+                                    setTimeout(function() {
+                                        imageElement.toggleClass('animate__animated animate__flipInX');
+                                    }, 0);
+
+                                });
+                                $('#hideButton2').click(function() {
+                                    var imageElement = $('#hideImage2');
+                                    var passwordField = $('#newPassword');
+                                    var passwordFieldType = passwordField.attr('type');
+
+                                    if (passwordFieldType === 'password') {
+                                        passwordField.attr('type', 'text');
+                                        imageElement.attr('src', '{{asset("./image/icon/view_grey_closed.png")}}')
+                                    } else {
+                                        passwordField.attr('type', 'password');
+                                        imageElement.attr('src', "{{asset('./image/icon/view_grey.png')}}")
+                                    }
+
+                                    imageElement.toggleClass('animate__animated animate__flipInX');
+                                    setTimeout(function() {
+                                        imageElement.toggleClass('animate__animated animate__flipInX');
+                                    }, 0);
+                                });
+                                $('#hideButton3').click(function() {
+                                    var imageElement = $('#hideImage3');
+                                    var passwordField = $('#newPasswordConfirm');
+                                    var passwordFieldType = passwordField.attr('type');
+
+                                    if (passwordFieldType === 'password') {
+                                        passwordField.attr('type', 'text');
+                                        imageElement.attr('src', '{{asset("./image/icon/view_grey_closed.png")}}')
+                                    } else {
+                                        passwordField.attr('type', 'password');
+                                        imageElement.attr('src', "{{asset('./image/icon/view_grey.png')}}")
+                                    }
+
+                                    imageElement.toggleClass('animate__animated animate__flipInX');
+                                    setTimeout(function() {
+                                        imageElement.toggleClass('animate__animated animate__flipInX');
+                                    }, 0);
+                                });
+                            });
+                        </script>
+                    </div>
+                    <div class="row text-center" style="margin-left: 128px; margin-right: 128px">
+                        <button type="submit" class="btn btn-primary">Update</button>
+                    </div>
+                </div>
+        </div>
+        <div class="card body bottom p-3 mt-2 gap-3">
+            <h5 class="mt-3 fw-bold">Key</h5>
+            <textarea name="privateKey" type="text" disabled>{{ $setting->privateKey }}</textarea>
+            <textarea name="publicKey" type="text" disabled>{{ $setting->publicKey }}</textarea>
+            <input name="encryptionKey" type="text" class="mb-3" value="{{ $setting->encryptionKey}}" disabled>
+
+            </form>
+        </div>
     </div>
-    <textarea name="privateKey" type="text" disabled>{{ $setting->privateKey }}</textarea>
-    <textarea name="publicKey" type="text" disabled>{{ $setting->publicKey }}</textarea>
-    <input name="encryptionKey" type="text" value="{{ $setting->encryptionKey}}" disabled>   
 
-</form>
+    <script>
+        $(document).ready(function() {
+            $('#hideButton1').click(function() {
+                var imageElement = $('#hideImage1');
+                var passwordField = $('#currentPassword');
+                var passwordFieldType = passwordField.attr('type');
 
+                if (passwordFieldType === 'password') {
+                    passwordField.attr('type', 'text');
+                    imageElement.attr('src', '{{asset("./image/icon/view_grey_closed.png")}}')
+                } else {
+                    passwordField.attr('type', 'password');
+                    imageElement.attr('src', "{{asset('./image/icon/view_grey.png')}}")
+                }
 
-<script>
-    $(document).ready(function() {
-        $('#hideButton1').click(function() {
-            var imageElement = $('#hideImage1');
-            var passwordField = $('#currentPassword');
-            var passwordFieldType = passwordField.attr('type');
-            
-            if (passwordFieldType === 'password') {
-            passwordField.attr('type', 'text');
-            imageElement.attr('src', '{{asset("./image/icon/view_grey_closed.png")}}')
-            } else {
-            passwordField.attr('type', 'password');
-            imageElement.attr('src', "{{asset('./image/icon/view_grey.png')}}")
-            }  
-
-            imageElement.toggleClass('animate__animated animate__flipInX');  
-            setTimeout(function() {
-                imageElement.toggleClass('animate__animated animate__flipInX');  
-            }, 1); 
+                imageElement.toggleClass('animate__animated animate__flipInX');
+                setTimeout(function() {
+                    imageElement.toggleClass('animate__animated animate__flipInX');
+                }, 1);
+            });
         });
-    });
-</script> 
+    </script>
+</div>
 @endsection
