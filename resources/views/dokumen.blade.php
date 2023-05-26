e248b2571e9eafad261f45fc8450ae3b88f09e99
RizkiPrtma1 <217006106@student.unsil.ac.id>
2023-05-26 23:31:15 +0700

    histori revisi

--- resources/views/dokumen.blade.php
+++ resources/views/dokumen.blade.php
@@ -73,28 +73,23 @@
           <td>{{ $val->keikutsertaan }}</td>
           @if ($message == 1)
           <td>
-            <button type="button" class="badge bg-success" data-bs-toggle="tooltip" data-bs-placement="top"
-              data-bs-custom-class="custom-tooltip" data-bs-title="Data Masih Sama Seperti pada Awal Dibuat.">
+            <button type="button" class="badge bg-success" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="Data Masih Sama Seperti pada Awal Dibuat.">
               Valid
             </button>
           </td>
           @else
           <td>
-            <button type="button" class="badge bg-danger" data-bs-toggle="tooltip" data-bs-placement="top"
-              data-bs-custom-class="custom-tooltip" data-bs-title="Data Sudah Tidak Sama Seperti Awal Dibuat.">
+            <button type="button" class="badge bg-danger" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="Data Sudah Tidak Sama Seperti Awal Dibuat.">
               Invalid
             </button>
           </td>
           @endif
-          <td scope="col"><button class="btn btn-primary btn-sm" data-bs-toggle="modal"
-              data-bs-target="#ModalQRC{{ $val->id }}">Lihat</button></td>
-          <td scope="col"><button class="btn btn-primary btn-sm" data-bs-toggle="modal"
-              data-bs-target="#ModalHapus{{ $val->id }}">Hapus</button></td>
+          <td scope="col"><button class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#ModalQRC{{ $val->id }}">Lihat</button></td>
+          <td scope="col"><button class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#ModalHapus{{ $val->id }}">Hapus</button></td>
         </tr>
 
         <!-- Modal QR Code -->
-        <div class="modal fade" id="ModalQRC{{ $val->id }}" tabindex="-1" aria-labelledby="exampleModalLabel"
-          aria-hidden="true">
+        <div class="modal fade" id="ModalQRC{{ $val->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
               <div class="modal-header">
@@ -112,23 +107,22 @@
               </div>
 
               <script>
-                $(document).ready(function(){
-                  $('#btnDownload{{$val->id}}').click(function() { 
+                $(document).ready(function() {
+                  $('#btnDownload{{$val->id}}').click(function() {
                     domtoimage.toBlob(document.getElementById('QRDownloadThis{{$val->id}}'))
-                    .then(function(blob){
-                      window.saveAs(blob, 'QRCode-{{$val->noPeserta}}.png');
-                    });
+                      .then(function(blob) {
+                        window.saveAs(blob, 'QRCode-{{$val->noPeserta}}.png');
+                      });
                   });
-                }); 
-              </script> 
+                });
+              </script>
 
             </div>
           </div>
         </div>
 
         <!-- Modal Hapus -->
-        <div class="modal fade" id="ModalHapus{{ $val->id }}" tabindex="-1" aria-labelledby="exampleModalLabel"
-          aria-hidden="true">
+        <div class="modal fade" id="ModalHapus{{ $val->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
               <div class="modal-header">
@@ -165,45 +159,45 @@
           </div>
 
           <script type="text/javascript">
-          $(document).ready(function() {
-            var decoder; // Declare the decoder variable outside the scope of the event handlers
+            $(document).ready(function() {
+              var decoder; // Declare the decoder variable outside the scope of the event handlers
 
-            // Open ModalScan event handler
-            $('#ModalScan').on('shown.bs.modal', function() {
-              var arg = {
-                resultFunction: function(result) {
-                  var redirect = '{{ url("/dokumen/check") }}';
-                  $.redirectPost(redirect, {
-                    uniqueId: result.code
-                  });
+              // Open ModalScan event handler
+              $('#ModalScan').on('shown.bs.modal', function() {
+                var arg = {
+                  resultFunction: function(result) {
+                    var redirect = '{{ url("/dokumen/check") }}';
+                    $.redirectPost(redirect, {
+                      uniqueId: result.code
+                    });
+                  }
+                };
+
+                // Initialize the decoder when the modal is opened
+                decoder = $("canvas").WebCodeCamJQuery(arg).data().plugin_WebCodeCamJQuery;
+                decoder.buildSelectMenu("select");
+                decoder.play();
+              });
+
+              // Close ModalScan event handler
+              $('#ModalScan').on('hidden.bs.modal', function() {
+                // Stop the decoder when the modal is closed
+                if (decoder) {
+                  decoder.stop();
                 }
-              };
+              });
 
-              // Initialize the decoder when the modal is opened
-              decoder = $("canvas").WebCodeCamJQuery(arg).data().plugin_WebCodeCamJQuery;
-              decoder.buildSelectMenu("select");
-              decoder.play();
+              $.extend({
+                redirectPost: function(location, args) {
+                  var form = '';
+                  $.each(args, function(key, value) {
+                    form += '<input type="hidden" name="' + key + '" value="' + value + '">';
+                  });
+                  $('<form action="' + location + '" method="GET">' + form + '</form>').appendTo('body')
+                    .submit();
+                }
+              });
             });
-
-            // Close ModalScan event handler
-            $('#ModalScan').on('hidden.bs.modal', function() {
-              // Stop the decoder when the modal is closed
-              if (decoder) {
-                decoder.stop();
-              }
-            });
-
-            $.extend({
-              redirectPost: function(location, args) {
-                var form = '';
-                $.each(args, function(key, value) {
-                  form += '<input type="hidden" name="' + key + '" value="' + value + '">';
-                });
-                $('<form action="' + location + '" method="GET">' + form + '</form>').appendTo('body')
-                  .submit();
-              }
-            });
-          });
           </script>
 
         </div>
@@ -238,23 +232,19 @@
               <label for="floatingInput">Instansi</label>
             </div>
             <div class="form-floating mb-3">
-              <input name="tanggalTerbit" type="date" class="form-control" id="floatingInput" placeholder="nama"
-                required>
+              <input name="tanggalTerbit" type="date" class="form-control" id="floatingInput" placeholder="nama" required>
               <label for="floatingInput">Tanggal Terbit</label>
             </div>
             <div class="form-floating mb-3">
-              <input name="noSertifikat" type="text" class="form-control" id="floatingInput" placeholder="nama"
-                required>
+              <input name="noSertifikat" type="text" class="form-control" id="floatingInput" placeholder="nama" required>
               <label for="floatingInput">No Sertifikat</label>
             </div>
             <div class="form-floating mb-3">
-              <input name="namaPelatihan" type="text" class="form-control" id="floatingInput" placeholder="nama"
-                required>
+              <input name="namaPelatihan" type="text" class="form-control" id="floatingInput" placeholder="nama" required>
               <label for="floatingInput">Nama Pelatihan</label>
             </div>
             <div class="form-floating mb-3">
-              <input name="keikutsertaan" type="text" class="form-control" id="floatingInput" placeholder="nama"
-                required>
+              <input name="keikutsertaan" type="text" class="form-control" id="floatingInput" placeholder="nama" required>
               <label for="floatingInput">Keikutsertaan</label>
             </div>
           </div>
@@ -269,7 +259,7 @@
 </div>
 
 <script>
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 </script>
 @endsection
--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -1,33 +1,32 @@
 @extends('layouts.layout_main')
 
-@section('content') 
+@section('content')
 
-<div class="row pt-3">
-    <!-- Kondisi jika terdapat data pada Log Audit -->
-    @if ($datas1->count() > 0)
-    <table id="example" class="table table-striped" style="width:100%;">
-        <thead>
-            <tr>
-                <th scope="col">No</th>
-                <th scope="col">Waktu</th>
-                <th scope="col">Aktifitas</th>
-            </tr>
-        </thead>
-        <tbody>
-            @php $i = 1; @endphp
-            @foreach($datas1 as $key=>$val)
-            <tr>
-                <td scope="col">{{ $i++ }}</td>
-                <td scope="col">{{ $val->created_at }}</td>
-                <td scope="col">{{ $val->aktifitas }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <!-- Kondisi jika tidak terdapat data pada Log Audit -->
-    @else
-    <h2>Waduh Kosong</h2>
-    @endif
+<div class="container">
+    <div class="row pt-3">
 
+
+        <table id="example" class="table table-striped" style="width:100%;">
+            <thead>
+                <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Waktu</th>
+                    <th scope="col">Aktifitas</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php $i = 1; @endphp
+                @foreach($datas1 as $key=>$val)
+                <tr>
+                    <td scope="col">{{ $i++ }}</td>
+                    <td scope="col">{{ $val->created_at }}</td>
+                    <td scope="col">{{ $val->aktifitas }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+
+    </div>
 </div>
 @endsection
